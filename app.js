2be5ad1d0bda20dfd2ef6de5571f37980465e884
weebdesign <155826765+weebdesign@users.noreply.github.com>
2024-08-04 01:21:21 +0600

    Update app.js

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ const startTimer=(duration)=>{
     const displayMinute= minute < 10? "0"+ minute:minute;
     const displaySecond= secound < 10? "0"+ secound : secound;
    
-     secound < 6 ? timerDisplay.style.color="red ": secound;
-     secound > 6 ? timerDisplay.style.color="black ": secound;
-        function changeOrage(){
+    // change color
+   
+    function changeOrage(){
         secound < 11 ? timerDisplay.style.color="yellow": secound;
       
         secound == 20 ?  playAudio(): secound;
@@ -31,15 +31,31 @@ const startTimer=(duration)=>{
        
         
     }
+    function changeRed(){
+       
+        //  secound > 6 ? timerDisplay.style.color="black ": secound;
+        //  secound > 11 ? timerDisplay.style.color="black ": secound;
+
+    }
+          secound > 11 ? timerDisplay.style.color="black ": secound;
+        //   secound > 11 ? playAudio(): secound;
+
+    timerDisplay.style.color=changeOrage();
+    timerDisplay.style.color=changeRed();
+    //  secound > 6 ? timerDisplay.style.color="black ": secound;
+
+    //  secound < 11 ? timerDisplay.style.color="orange ": secound;
+    //  secound > 11 ? timerDisplay.style.color="black ": secound;
 
     timerDisplay.textContent=displayMinute+':'+displaySecond;
     if(--timer <0){
+       
         clearInterval(timerInterval);
         timerDisplay.textContent='Breath Out'
         setTimeout(() => {
             startTimer(timeLeft);
-             timerDisplay.textContent='Breath In'
             playAudioTwo()
+             timerDisplay.textContent='Breath In'
             
         },5000);
     }
@@ -57,7 +73,7 @@ const startTimer=(duration)=>{
 
 const stopTimer =()=>{
     clearInterval(timerInterval);
-    timerDisplay.textContent='Breath In'
+    timerDisplay.textContent='Breat In'
     isRunning=false;
 
 }
@@ -78,7 +94,9 @@ startBtn.addEventListener("click",function(){
 stopBtn.addEventListener("click",function(){
     stopTimer();
     isRunning=false;
-    const audio=document.getElementById('audio');
+  
+});
+const audio=document.getElementById('audio');
 const audioTwo=document.getElementById('audioTwo');
 
 function playAudio(){
@@ -88,8 +106,5 @@ function playAudio(){
 function playAudioTwo(){
     audioTwo.play()
 }
-  
-});
-
 
 
